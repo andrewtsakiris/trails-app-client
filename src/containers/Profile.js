d8bb38719477786c188b6e004aacb89e139666b1
Andrew Tsakiris <andrewtsakiris@Andrews-MacBook-Pro.local>
2019-05-31 15:09:34 -0700

    starting profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Profile.css";
 import { Link } from "react-router-dom";
+import {API} from "aws-amplify";
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completedTrails: [],
+      savedTrails: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const completedTrails = API.get("trails", "listtrails/completed");
+      const savedTrails = API.get("trails", "trails/saved");
+      this.setState({
+        completedTrails,
+        savedTrails
+      });
+    } catch(e) {
+
+    }
+  }
+
+  renderList(sTrails, cTrails) {
+    return (
+      0
+    );
+  }
+
   render() {
     return (
       <div className="Profile">
-        <div className="lander">
-          <h1>Trail Hero</h1>
-          <p>get out there</p>
-          <Link to="/search">Find Trails</Link>
-        </div>
+        <Link to="/search">Find Trails</Link>
+        <PageHeader>Your Profile Home Page</PageHeader>
+        {this.renderList(this.state.savedTrails, this.state.completedTrails)}
       </div>
     );
   }
