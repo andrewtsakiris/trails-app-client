4d7ce63579ce9c734a3c431fb9df946a014b7348
Nainika D'Souza <nainikadsouza@Nainikas-MacBook-Pro.local>
2019-05-31 14:37:17 -0700

    login/out

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,18 @@ import Profile from "./containers/Profile";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import Search from "./containers/Search";
+import AppliedRoute from "./components/AppliedRoute";
 
 
-export default () =>
-  <Switch>
-    <Route path="/" exact component={Profile} />
-    <Route path="/login" exact component={Login}/>
-    <Route path="/signup" exact component={Signup}/>
-    <Route path="/search" exact component={Search}/>
-  </Switch>;
+
+
+export default ({ childProps }) =>
+<Switch>
+  <AppliedRoute path="/" exact component={Profile} props={childProps} />
+  <AppliedRoute path="/login" exact component={Login} props={childProps} />
+  <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+  <AppliedRoute path="/search" exact component={Search} props={childProps} />
+  { /* Finally, catch all unmatched routes */ }
+  {/* <Route component={NotFound} /> */}
+</Switch>;
+
